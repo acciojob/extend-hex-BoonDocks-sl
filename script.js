94d4914eebfa3ae58f826f760f76b5ce2208dee6
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-10 12:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	 shortHex = shortHex.replace('#', '');
+
+  const red = shortHex[0];
+  const green = shortHex[1];
+  const blue = shortHex[2];
+
+  const fullHex = `#${red}${red}${green}${green}${blue}${blue}`;
+
+  return fullHex;
 };
 
 // Do not change the code below.
